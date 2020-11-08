7ffe7fd58ccdcd3708cd072085613882ae4cfe61
angkrz89 <angkrz89@users.noreply.github.com>
2020-11-08 14:58:11 +0100

    Zadania python

---
+++ prog15.py
@@ -0,0 +1,12 @@
+# zaimplementowac - * /
+def calculator(a, b, op):
+    if op == '+':
+        return a + b
+    return 0
+
+if __name__ == '__main__':
+    c = calculator(1, 2, '+')
+    print(c)
+    d = calculator(10, 200, '-')
+    print(d)
+    
